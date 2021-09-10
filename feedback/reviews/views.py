c6be7f29ea4430480ec7de9c13e0c41004fdcbf6
Djonathan Goulart <d.goulart@outlook.com.br>
2021-09-09 22:46:43 -0300

    course section 11: class views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,48 +1,36 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import ReviewForm
+from .models import Review
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, "reviews/review.html", {"form": form})
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
-# def review(request):
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
 
-#     if request.method == "POST":
-#         form = ReviewForm(request.POST)
-
-#         # to update an existing entity
-#         # existing = Review.objects.get(pk=pk)
-#         # form = ReviewForm(request.POST, instance=existing)
-
-#         if form.is_valid():
-#             # when using Form
-#             # new_review = Review(**form.cleaned_data)
-#             # new_review.save()
-
-#             # when using ModelFOrm
-#             form.save()
-
-#             return HttpResponseRedirect("/thank-you")
-
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, "reviews/review.html", {"form": form})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Thank You!!"
+        return context
 
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ReviewListView(ListView):
+    template_name = "reviews/reviews_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+
+class ReviewDetailView(DetailView):
+    template_name = "reviews/review_detail.html"
+    model = Review
+    context_object_name = "review"
